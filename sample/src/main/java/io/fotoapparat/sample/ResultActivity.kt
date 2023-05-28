decd6c4ec60e192b318263d827d200fc7662d396
Name <sirotenkostepan@mail.ru>
2023-05-28 23:13:26 +0700

    webView2

--- sample/src/main/java/io/fotoapparat/sample/ResultActivity.kt
+++ sample/src/main/java/io/fotoapparat/sample/ResultActivity.kt
@@ -1,31 +1,33 @@
 package io.fotoapparat.sample
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.os.Handler
-import android.os.Looper
-import android.util.Log
+import android.webkit.ValueCallback
 import android.webkit.WebView
-import android.webkit.WebViewClient
 import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 import io.fotoapparat.sample.constance.Constance
-import org.jsoup.Jsoup
-import org.w3c.dom.Document
-import java.io.IOException
 
 class ResultActivity : AppCompatActivity() {
     private lateinit var webView: WebView
     private lateinit var number: String
-
+    private lateinit var script: String
+    private lateinit var carName: TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_result)
+
+        carName = findViewById(R.id.carName)
 
         webView = findViewById(R.id.webView)
         number = intent.getStringExtra(Constance.INTENT_KEY_1)
 
         webView.loadUrl("https://avtocod.ru/proverkaavto/${number}?rd=GRZ")
         webView.settings.javaScriptEnabled = true
-        webView.webViewClient = WebViewClient()
+        webView.settings.domStorageEnabled = true
+
+        webView.evaluateJavascript("(f() return{document.querySelector(\".report-header__title\").innerText})()"
+        ) { s ->
+            carName.text = s
+        }
     }
 }
